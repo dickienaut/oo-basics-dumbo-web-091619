5c5e05301d15655d5f09912f48075bd666923572
lyle dickie <lyle.e.dickie@gmail.com>
2019-09-18 20:29:53 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,9 +2,11 @@ class Book
   attr_reader :title, :author
   def initialize(title)
     @title = title
-    @author = "Agatha Christie"
-    # @page_count = page_count
-    # @genre = genre
   end
+  
+  def author=(author)
+    @author = author
+  end
+  
 end
 
