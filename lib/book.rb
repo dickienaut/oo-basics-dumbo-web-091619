d33c0063eba549cb8669d97b3ecf7d2d5b4674c9
lyle dickie <lyle.e.dickie@gmail.com>
2019-09-18 20:25:37 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,7 +1,10 @@
 class Book
   attr_reader :title
-  def initialize(title)
+  def initialize(title, author_name, page_count, genre)
     @title = title
+    @author = author_name
+    @page_count = page_count
+    @genre = genre
   end
 end
 
